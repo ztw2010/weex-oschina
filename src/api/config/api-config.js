1fa9d5f9ac8f42873a01b17124a02a86722c1115
ztw2010 <wlgc2010wlgc@sina.cn>
2017-11-02 16:39:18 +0800

    项目提交

--- src/api/config/api-config.js
+++ src/api/config/api-config.js
@@ -1,12 +1,12 @@
 const key_config = {
     app_key: '4uSlcQ0R4OOK2aW82aLJ',
     app_secret: 'FkOf5Z3Jh7Z0ZPCFkiEGUZIqtIpQMM9o',
-    redirect_uri: 'http://192.168.2.199:8089/splash'
+    redirect_uri: 'http://192.168.2.22:8083/splash'
 }
 
 const host_config = {
-    local: 'http://192.168.2.199:8089/',
-    host: 'http://192.168.2.199:8083/vueoschina',
+    local: 'http://192.168.2.22:8083/',
+    host: 'http://192.168.2.22:8083/vueoschina',
     oauth: 'https://www.oschina.net/action/oauth2/authorize',
 }
 
